37cabfe408456e75558cc5f07ee3643184d2f117
程伟 <chengwei10@centaline.com.cn>
2017-04-11 17:53:20 +0800

    添加项目文件。

---
+++ Spring.Net.Demo.Lesson1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Spring.Context;
+using Spring.Context.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spring.Net.Demo.Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IoCMethod();  // IoC方法"
+
+            Console.ReadLine();
+        }
+
+        private static void IoCMethod()
+        {
+            IApplicationContext ctx = ContextRegistry.GetContext();
+            IPersonDao dao = ctx.GetObject("PersonDao") as IPersonDao;
+            if (dao != null)
+            {
+                dao.Save();
+                Console.WriteLine("我是IoC方法");
+            }
+        }
+    }
+}
---
+++ Spring.Net.Demo.Lesson2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Spring.Context;
+using Spring.Context.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spring.Net.Demo.Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppRegistry();
+            Console.ReadLine();
+        }
+
+        static void AppRegistry()
+        {
+            IApplicationContext ctx = ContextRegistry.GetContext();
+            Console.WriteLine(ctx.GetObject("PersonDao").ToString());
+        }
+    }
+}
